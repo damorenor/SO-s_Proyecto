08fbf4a7be1b464e18db13035cf39757476e3c2d
damorenor <39134774+damorenor@users.noreply.github.com>
2019-05-01 19:25:39 -0500

    Add files via upload

---
+++ p1-dogProgram.c
@@ -0,0 +1,55 @@
+//damorenor + jocarrilloco
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 1e8;
+
+//Estructura que almacena la info de la mascota
+struct dogType
+{
+	char nombre [32];
+	char tipo [32];
+	int edad;
+	char raza [16];
+	int estatura;
+	float peso;
+	char sexo;
+};
+
+typedef struct dogType pet;//Se define tipo de dato pa ahorrar codigo :D
+
+//Inicialización del registro de mascotas
+void buildRegister(){
+	printf("Se creó el registro\n");
+}
+
+//Carga el menu
+void menu(){
+	//Se imprime en consola el menú de usuario
+	system("clear");
+	printf("\t\t\tVET CLINIC :3\n"
+			"\t1.\tIngresar Registro\n\n\n"
+			"\t2.\tVer Registro\t\n"
+			"\t3.\tBorrar Registro\n"
+			"\t4.\tBuscar Registro\n"
+			"\t5.\tSalir\n"
+		);
+	//Se recibe por teclado un entero que corresponde a la funcionalidad escogida
+	short option;
+	scanf("%hd",&option);
+}
+
+//Ejecución permanente del programa
+void run(){
+	while(1){
+		menu();
+	}
+}
+
+//main
+int main(int argc, char const *argv[])
+{
+	buildRegister();
+	run();
+	return 0;
+}
