d047680610694a1add56551701f54a90448a9afb
damorenor <39134774+damorenor@users.noreply.github.com>
2019-05-01 22:48:12 -0500

    Update nombresArray.c

--- nombresArray.c
+++ nombresArray.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void nameArrayGenerator(char mainNames[1716][33]){
+void nameArrayGenerator(char mainNames[1716][32]){
 	FILE *names;
 	names = fopen("nombresBinario.dat","r");
 	if(names == NULL){
@@ -10,7 +10,7 @@ void nameArrayGenerator(char mainNames[1716][33]){
 		exit(-1);
 	}
 	int check;
-	char *buffer;
+	char buffer[32];
 	for(int i=0;i<1;i++){
 		check = fseek(names,sizeof(buffer)*i,SEEK_SET);
 		if(check == -1){
@@ -24,7 +24,7 @@ void nameArrayGenerator(char mainNames[1716][33]){
 			exit(-1);
 		}
 
-		mainNames[i] =  buffer;//el problema es en esta linea
+		strcpy(mainNames[i], buffer);//el problema es en esta linea
 	}
 	fclose(names);
 }
@@ -32,7 +32,7 @@ void nameArrayGenerator(char mainNames[1716][33]){
 
 int main(int argc, char const *argv[])
 {
-	char mainNames[1716][33];//crear una constante pa esta monda :D
+	char mainNames[1716][32];//crear una constante pa esta monda :D
 	nameArrayGenerator(mainNames);
 	printf("%s\n", mainNames[0]);
 	return 0;
