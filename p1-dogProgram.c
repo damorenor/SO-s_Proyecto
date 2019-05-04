739fbf7c384adbb9d87a78ae1aafcd24d3fbf6fc
joacarrilloco <joacarrilloco@unal.edu.co>
2019-05-03 21:39:48 -0500

    good road

--- p1-dogProgram.c
+++ p1-dogProgram.c
@@ -1,30 +1,43 @@
 //damorenor + joacarrilloco
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-#define REGISTER_SIZE int(1e8 + 10)
+#define REGISTER_SIZE ( (int) 1e7 ) 
 #define NOMBRE_SIZE 32
 #define TIPO_SIZE 32
 #define RAZA_SIZE 16
+#define TOTAL_NAMES 1716
+#define TOTAL_BREEDS 13
 
-//Estructura que almacena la info de la mascota
+#define HASH_SIZE 10007 
+#define MOD 256
+
 struct dogType
 {
-	char nombre [NOMBRE_SIZE];
-	char tipo [TIPO_SIZE];
-	int edad;
-	char raza [RAZA_SIZE];
-	int estatura;
-	float peso;
-	char sexo;
+        unsigned char nombre [NOMBRE_SIZE];
+        unsigned char tipo [TIPO_SIZE];
+        int edad;
+        unsigned char raza [RAZA_SIZE];
+        int estatura;
+        float peso;
+        unsigned char sexo;
+
+	int idPrev;
 };
 
-typedef struct dogType pet;//Se define tipo de dato pa ahorrar codigo :D
 
-//Inicialización del registro de mascotas y carga de datos necesarios
-void init(){
-	printf("Se creó el registro\n");
-}
+void initPointers();
+void loadPointers();
+void savePointers();
+void enterPet();
+void seePet();
+void deletePet();
+void searchPet();
+
+int * lastID;
+
 
 //Carga el menu
 void menu(){
@@ -43,31 +56,124 @@ void menu(){
 	scanf("%hd",&option);
 	switch(option){
 		case 1:
-		enterPet();
+			enterPet();
+			break;
 		case 2:
-		seePet();
+			seePet();
+			break;
 		case 3:
-		deletePet();
+			deletePet();
+			break;
 		case 4:
-		searchPet();
+			searchPet();
+			break;
 		case 5:
-		exit(-1);
+			
+			savePointers();
+			exit( 0 );
+
 		default:
 		printf("ingrese una opción valida\n");
 	}	
 }
 
-//Ejecución permanente del programa
 void run(){
 	while(1){
 		menu();
+
 	}
 }
 
-//main
-int main(int argc, char const *argv[])
+int main()
 {
-	init();
+	initPointers();
+	loadPointers();	
 	run();
 	return 0;
+}
+
+
+void initPointers()
+{
+	lastID = ( int * ) malloc ( HASH_SIZE * sizeof ( int ) );
+	if( lastID == NULL )
+	{
+		perror("error en el malloc lastID" );
+		exit( -1 );
+	}
+        memset( lastID, -1, HASH_SIZE * sizeof ( int ) );
+}
+
+
+void loadPointers()
+{
+	FILE * points;
+	points = fopen("dataPointers.dat", "r" );
+
+	int i, check;
+	for( i = 0; i < HASH_SIZE; ++ i )
+	{
+		check = fseek(points, i * sizeof(int),SEEK_SET);
+		if( check == -1 )
+		{
+			perror("error seek Pointers");
+			exit( -1 );
+		}
+		check = fread(&lastID[i], sizeof(int), 1, points );
+		
+		if( check == 0 )
+		{
+			perror("error en lectura Pointers");
+			exit(-1);
+		}
+	}
+	fclose( points );
+}
+
+
+void savePointers(){
+        FILE *points;
+        int  check;
+
+        points = fopen("dataPointers.dat","w+");
+        if(points == NULL){
+                perror("error generando apuntadores");
+                exit(-1);
+        }
+
+        for (int i = 0; i < HASH_SIZE; ++i){
+                check = fwrite(&lastID[i], sizeof(int),1,points);
+                if (check == 0){
+                        perror("error escribiendo dataPointers");
+                }
+        }
+        fclose(points);
+}
+
+
+void enterPet()
+{
+
+
+}
+
+
+void seePet()
+{
+
+
+}
+
+
+void deletePet()
+{
+
+
+}
+
+
+void searchPet()
+{
+
+
 }
